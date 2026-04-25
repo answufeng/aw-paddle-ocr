552b37fefcc81f6d9c5ae127300eb076605afda2
lianwufeng <ail36413@163.com>
2026-04-25 22:42:42 +0800

    fix: add missing TextView import in MainActivity

--- demo/src/main/java/com/answufeng/paddleocr/demo/MainActivity.kt
+++ demo/src/main/java/com/answufeng/paddleocr/demo/MainActivity.kt
@@ -3,7 +3,7 @@ package com.answufeng.paddleocr.demo
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
